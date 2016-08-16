330d4473c1e5fc051d26fa987136fde7c2d19964
Matthew Miner <matthew@matthewminer.com>
2016-08-15 21:11:13 -0700

    Initial Commit

---
+++ Scratchpad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scratchpad
+//
+//  Created by Matthew Miner on 2016-08-15.
+//  Copyright © 2016 Matthew Miner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Scratchpad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scratchpad
+//
+//  Created by Matthew Miner on 2016-08-15.
+//  Copyright © 2016 Matthew Miner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
